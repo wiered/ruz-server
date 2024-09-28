69fa5db4c9e072d673fa79b247aa21093995e9c9
Artem <wieredsh@gmail.com>
2024-09-28 03:46:05 +0300

    Added models

---
+++ src/models/models.py
@@ -0,0 +1,29 @@
+﻿import dataclasses
+from datetime import datetime
+
+@dataclasses.dataclass
+class User:
+    user_id: int
+    group_id: int
+    group_name: str
+    sub_group: int
+    is_premium: bool
+
+@dataclasses.dataclass
+class Group:
+    group_id: int
+    group_name: str
+
+@dataclasses.dataclass
+class Lesson:
+    discipline: str
+    kind_of_work: str
+    auditorium: str
+    date: datetime
+    beginLesson: datetime
+    endLesson: datetime
+    lecturer_title: str
+    lecturer_rank: str
+    sub_group: int
+    group_id: int
+    id: int = None
