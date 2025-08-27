3ba5a6cdd828772027646e13e8602019e4358a41
Artem <wieredsh@gmail.com>
2025-08-27 22:14:16 +0300

    Add docstrings to kind_of_work_repository

--- src/repositories/kind_of_work_repository.py
+++ src/repositories/kind_of_work_repository.py
@@ -14,25 +14,70 @@ class KindOfWorkRepository:
         self.session = session
 
     def Create(self, kind_of_work: KindOfWork):
+        """
+        Creates a new KindOfWork.
+
+        Args:
+            kind_of_work (KindOfWork): Kind of work to be created.
+
+        Returns:
+            KindOfWork: Created KindOfWork.
+        """
         self.session.add(kind_of_work)
         self.session.commit()
         return kind_of_work
 
     def GetOrCreate(self, kind_of_work: KindOfWork):
+        """
+        Creates a new KindOfWork if it doesn't exist, otherwise it retrieves the existing one.
+
+        Args:
+            kind_of_work (KindOfWork): Kind of work to be created or retrieved.
+
+        Returns:
+            KindOfWork: Created or retrieved KindOfWork.
+        """
         existing = self.GetById(kind_of_work.id)
         if existing:
             return existing
         return self.Create(kind_of_work)
 
     def ListAll(self):
+        """
+        Gets all KindOfWork objects from the database.
+
+        Returns:
+            List[KindOfWork]: List of all KindOfWork objects.
+        """
         stmt = select(KindOfWork)
         return self.session.exec(stmt).all()
 
     def GetById(self, value: int):
+        """
+        Gets a KindOfWork object from the database by its ID.
+
+        Args:
+            value (int): ID of the KindOfWork object to be retrieved.
+
+        Returns:
+            KindOfWork: Retrieved KindOfWork object or None if no such object exists.
+        """
         stmt = select(KindOfWork).where(KindOfWork.id == value)
         return self.session.exec(stmt).first()
 
     def Update(self, value: int, type_of_work: str, complexity: int):
+        """
+        Updates a KindOfWork object in the database.
+
+        Args:
+            value (int): ID of the KindOfWork object to be updated.
+            type_of_work (str): New type of work.
+            complexity (int): New complexity.
+
+        Returns:
+            bool: True if the update was successful, False otherwise.
+        """
+
         try:
             stmt = update(KindOfWork).where(KindOfWork.id == value).values(
                 type_of_work=type_of_work, complexity=complexity
@@ -45,6 +90,16 @@ class KindOfWorkRepository:
             return False
 
     def Delete(self, value: int) -> bool:
+        """
+        Deletes a KindOfWork object from the database by its ID.
+
+        Args:
+            value (int): ID of the KindOfWork object to be deleted.
+
+        Returns:
+            bool: True if the deletion was successful, False otherwise.
+        """
+
         try:
             stmt = delete(KindOfWork).where(KindOfWork.id == value)
             result = self.session.exec(stmt)
