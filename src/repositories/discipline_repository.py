996101d61aed8550ac708e78b22a07aaafb5124b
Artem <wieredsh@gmail.com>
2025-08-28 00:13:47 +0300

    Fix importing models to repositories

--- src/repositories/discipline_repository.py
+++ src/repositories/discipline_repository.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import selectinload
 from sqlmodel import Session, delete, select, update
 
-from src.models import Discipline
+from models import Discipline
 
 class DisciplineRepository:
     def __init__(self, session: Session):
