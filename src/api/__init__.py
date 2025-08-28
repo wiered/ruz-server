976cca6bce1bbc65b55b4bced2ab730d36fd5882
Artem <wieredsh@gmail.com>
2025-08-28 07:31:19 +0300

    feat(api): include lecturer routes

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,8 +1,9 @@
 ﻿from fastapi import APIRouter
 from api.groups import router as groups_router
 from api.users import router as users_router
+from api.lecturers import router as lecturers_router
 
 api_router = APIRouter()
 api_router.include_router(groups_router, tags=["group"])
 api_router.include_router(users_router, tags=["user"])
-
+api_router.include_router(lecturers_router, tags=["lecturer"])
