b20f4de7ffb519fded46e6ac03c2e675ffb641c8
Artem <wieredsh@gmail.com>
2025-08-28 07:32:17 +0300

    feat(app): Configure logging for project

--- src/api/app.py
+++ src/api/app.py
@@ -1,14 +1,15 @@
 ﻿import os
 import logging
+from logging.config import dictConfig
 
 from fastapi import FastAPI, Request, Security
 from api.security import require_api_key
 from api import api_router
+from logging_config import LOGGING_CONFIG
 
-logging.basicConfig(
-    format="%(levelname)s: %(asctime)s %(name)s %(message)s",
-    level=logging.INFO,
-)
+# Setup logging
+dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 app.include_router(api_router, prefix="/api")
@@ -19,9 +20,10 @@ async def root(request: Request):
 
 @app.get("/public")
 async def public():
+    logger.info("public ok")
     return {"message": "public ok"}
 
 @app.get("/protected")
 async def protected(_api_key: str = Security(require_api_key)):
-    # сюда попадём только с валидным ключом
+    logger.info("protected ok")
     return {"message": "protected ok"}
