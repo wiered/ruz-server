fcbe063a6df0dbefb39fa45572240b2a9c74da6c
Artem <wieredsh@gmail.com>
2025-08-29 07:10:36 +0300

    feat(api): auditorium endpoints

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,6 +4,7 @@ from api.users import router as users_router
 from api.lecturers import router as lecturers_router
 from api.kind_of_work import router as kind_of_work_router
 from api.discipline import router as discipline_router
+from api.auditorium import router as auditorium_router
 
 api_router = APIRouter()
 api_router.include_router(groups_router, tags=["group"])
@@ -11,3 +12,4 @@ api_router.include_router(users_router, tags=["user"])
 api_router.include_router(lecturers_router, tags=["lecturer"])
 api_router.include_router(kind_of_work_router, tags=["kind_of_work"])
 api_router.include_router(discipline_router, tags=["discipline"])
+api_router.include_router(auditorium_router, tags=["auditorium"])
