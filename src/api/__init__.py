104a9d461472c6e7d03ac8ea567572e199eb1abf
Artem <wieredsh@gmail.com>
2025-08-29 07:01:00 +0300

    feat(api): add discipline endpoints

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,8 +2,12 @@
 from api.groups import router as groups_router
 from api.users import router as users_router
 from api.lecturers import router as lecturers_router
+from api.kind_of_work import router as kind_of_work_router
+from api.discipline import router as discipline_router
 
 api_router = APIRouter()
 api_router.include_router(groups_router, tags=["group"])
 api_router.include_router(users_router, tags=["user"])
 api_router.include_router(lecturers_router, tags=["lecturer"])
+api_router.include_router(kind_of_work_router, tags=["kind_of_work"])
+api_router.include_router(discipline_router, tags=["discipline"])
